72ccea2c448756890c60c6af010fc7958add7779
itsrirer <riderits57@gmail.com>
2026-01-28 23:43:26 +0530

    Update BOT_TOKEN and SUPER_ADMINS
    Updated BOT_TOKEN and changed a super admin ID.

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN") or "YOUR_BOT_TOKEN_HERE"
+BOT_TOKEN = os.getenv("BOT_TOKEN") or "8239490595:AAE2lUq4s-UfezdXMCq7Q6DPRgrF2ZF5BB0"
 
 # yahan apna Telegram user id daalo (super admin)
 SUPER_ADMINS = {
-    7445620075,  # <-- replace with your user id
+    8438661030,  # <-- replace with your user id
 }
 
 # agar unknown topic mile to jis thread me bhejna ho (General)
